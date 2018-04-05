876f2ab141fd03e8de34170a682f5b3786dfc171
KKSrinivasan <kksrini89@gmail.com>
2018-04-05 18:05:24 +0530

    Lazy loading module is implemented with NoteModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { Routing } from './app.routing';
 import { AppComponent } from './app.component';
+import { NoteModule } from './notes/note.module';
 import { AboutComponent } from './shared/components/about/about.component';
 import { HomeComponent } from './shared/components/home/home.component';
 
@@ -13,7 +14,8 @@ import { HomeComponent } from './shared/components/home/home.component';
   ],
   imports: [
     BrowserModule,
-    Routing
+    Routing,
+    NoteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,7 +6,8 @@ import { HomeComponent, AboutComponent} from './shared/index';
 const APP_Routes: Routes =[
     { path: '', component: HomeComponent },
     { path: 'notes', loadChildren:'./notes/note.module#NoteModule'},
-    { path: 'about', component: AboutComponent }
+    { path: 'about', component: AboutComponent },
+    { path: '**', redirectTo: '' , pathMatch:'full' }
 ]
 
 export const Routing: ModuleWithProviders = RouterModule.forRoot(APP_Routes);
